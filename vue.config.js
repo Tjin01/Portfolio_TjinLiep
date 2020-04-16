17aa170609a70c29ca1dadce3a62e48ef57beeed
Donavan Tjin <donovan__tls@hotmail.com>
2020-04-16 12:08:24 +0200

    portfolio done

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
-const path = require('path')
-
+const path = require('path');
 
 module.exports = {
-
+    outputDir: path.resolve(__dirname, '../public'),
+    devServer: {
+        proxy: {
+          '^/api': {
+            target: 'http://localhost:3000',
+            ws: true,
+            changeOrigin: true
+        },
+      }
+    }
 }
